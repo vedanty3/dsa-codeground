f559cc74c5c8cfd2ec26642c221c35e44c1bcd92
Vedant Yetekar <ursvedantyetekar@gmail.com>
2022-03-20 19:52:48 +0530

    added permutations second approach

--- problem solving/leetcode/Permutations.cpp
+++ problem solving/leetcode/Permutations.cpp
@@ -41,3 +41,39 @@ public:
         return ans;
     }
 };
+
+/*
+// Better Approach
+
+class Solution {
+public:
+
+    void findPermutations(vector<int>&nums, vector<vector<int>>&ans, int idx)
+    {
+        if(idx==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = idx; i<nums.size(); i++)
+        {
+            swap(nums[i], nums[idx]);
+            findPermutations(nums, ans, idx+1);
+            swap(nums[i], nums[idx]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>>ans;
+
+        int idx = 0;
+
+        findPermutations(nums, ans, idx);
+
+        return ans;
+
+    }
+};
+*/
