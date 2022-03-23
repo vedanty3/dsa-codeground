6ffe3ba74cbeb87e323d5f9c14654000556a6b8b
Vedant Yetekar <ursvedantyetekar@gmail.com>
2022-03-23 15:45:53 +0530

    added new code

--- js/12-Numbers-Dates-Timers-Bankist/script.js
+++ js/12-Numbers-Dates-Timers-Bankist/script.js
@@ -342,4 +342,19 @@ labelBalance.addEventListener("click", function () {
   });
 });
 
+
+// Numeric Operator
+const diameter = 287_460_000_000_000;
+console.log(diameter);
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee = 15_00;
+console.log(transferFee);
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number("230_000"));
+
 */
