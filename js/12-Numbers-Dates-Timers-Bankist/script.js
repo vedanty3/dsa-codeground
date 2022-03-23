4fa173e1d1eb17ffce98a8558da4078db55bb5a7
Vedant Yetekar <ursvedantyetekar@gmail.com>
2022-03-23 15:36:00 +0530

    added new problem

--- js/12-Numbers-Dates-Timers-Bankist/script.js
+++ js/12-Numbers-Dates-Timers-Bankist/script.js
@@ -89,7 +89,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -99,19 +99,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter((mov) => mov > 0)
@@ -121,7 +121,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -201,7 +201,7 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (
     amount > 0 &&
@@ -278,5 +278,68 @@ console.log(Number.isInteger("20"));
 console.log(Number.isInteger(+"20"));
 console.log(Number.isInteger(+"20X"));
 console.log(Number.isInteger(20 / 0));
+
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(22, 1, 22, 99, 4, 5));
+console.log(Math.min(22, 1, 22, 99, 4, 5));
+console.log(Math.PI * Number.parseFloat("10px") ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() ** (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
+
+// Rounding
+console.log(Math.trunc(2.22));
+console.log(Math.round(2.22));
+console.log(Math.round(2.49));
+console.log(Math.round(2.79));
+console.log(Math.ceil(2.44));
+console.log(Math.ceil(2.1));
+console.log(Math.ceil(2.7));
+console.log(Math.floor(2.7));
+console.log(Math.floor("2.7"));
+
+console.log(Math.trunc(-2.22));
+console.log(Math.round(-2.79));
+console.log(Math.ceil(-2.44));
+console.log(Math.floor(-2.7));
+
+// Decimals
+console.log((2.7).toFixed(0));
+console.log(Math.PI.toFixed(48));
+console.log((2.767).toFixed(2));
+console.log((+"2.767").toFixed(2));
+console.log((+"2.767").toExponential(2));
+
+
+// Remainder Operator
+
+console.log(5 % 2);
+console.log(8 % 2);
+
+const isEven = (n) => {
+  return n % 2 == 0;
+};
+
+console.log(isEven(6));
+console.log(isEven(9));
+
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = "orangered";
+    }
+    if (i % 3 === 0) {
+      row.style.backgroundColor = "blue";
+    }
+  });
+});
 
 */
