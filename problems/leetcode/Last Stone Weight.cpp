ea38dae104381eeda752a874150cd82195d0987c
vedant <ursvedantyetekar@gmail.com>
2022-04-07 16:11:55 +0530

    added daily challenge

---
+++ problems/leetcode/Last Stone Weight.cpp
@@ -0,0 +1,23 @@
+// https : // leetcode.com/problems/last-stone-weight/
+
+class Solution
+{
+public:
+    int lastStoneWeight(vector<int> &stones)
+    {
+
+        priority_queue<int> pq(stones.begin(), stones.end());
+
+        while (pq.size() > 1)
+        {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+
+            pq.push(abs(y - x));
+        }
+
+        return pq.top();
+    }
+};
