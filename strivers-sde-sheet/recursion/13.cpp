482ca6bbe9ac0ed0bf9f8500d357e5e0957fe0d3
vedant <ursvedantyetekar@gmail.com>
2022-04-21 22:59:53 +0530

    updated recursion code

---
+++ strivers-sde-sheet/recursion/13.cpp
@@ -0,0 +1,39 @@
+// Print the subsets whose sum is k (repetition is allowed).
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void subsetSum(int arr[], vector<int> &ds, int idx, int k, int n)
+{
+    if (idx == n)
+    {
+        if (k == 0)
+        {
+            for (auto i : ds)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+
+        return;
+    }
+
+    if (arr[idx] <= k)
+    {
+        ds.push_back(arr[idx]);
+        subsetSum(arr, ds, idx, k - arr[idx], n);
+        ds.pop_back();
+    }
+
+    subsetSum(arr, ds, idx + 1, k, n);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3};
+    vector<int> ds;
+    subsetSum(arr, ds, 0, 4, 3);
+
+    return 0;
+}
