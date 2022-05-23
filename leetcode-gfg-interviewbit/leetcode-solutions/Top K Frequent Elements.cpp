3d0d28f836b6e85a1b0ebf35f8a83deabdbaa428
vedant <ursvedantyetekar@gmail.com>
2022-05-24 02:07:57 +0530

    added leetcode soln

--- leetcode-gfg-interviewbit/leetcode-solutions/Top K Frequent Elements.cpp
+++ leetcode-gfg-interviewbit/leetcode-solutions/Top K Frequent Elements.cpp
@@ -3,10 +3,9 @@
 class Solution
 {
 public:
-    vector<int> topKFrequent(vector<int> &nums, int k)
+    vector<int> topK(vector<int> &nums, int k, vector<int> &ans)
     {
-
-        vector<int> ans;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         unordered_map<int, int> m;
 
         for (auto i : nums)
@@ -14,18 +13,31 @@ public:
             m[i]++;
         }
 
-        priority_queue<pair<int, int>> pq;
-
         for (auto i : m)
         {
             pq.push(make_pair(i.second, i.first));
+
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
         }
 
-        while (k--)
+        while (!pq.empty())
         {
             ans.push_back(pq.top().second);
             pq.pop();
         }
+
+        return ans;
+    }
+
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+
+        vector<int> ans;
+
+        topK(nums, k, ans);
 
         return ans;
     }
