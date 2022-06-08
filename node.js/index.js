1c84903c57fc14134b32f4e68c65a84c275d6940
vedant <ursvedantyetekar@gmail.com>
2022-06-08 23:45:22 +0530

    added node.js code_ground

--- node.js/index.js
+++ node.js/index.js
@@ -1,5 +1,54 @@
-// console.log("hello world");
+/*
+const app = require("./app");
+
+console.log("hello world");
 
 let a = 20;
 let b = 30;
 console.warn(a + b);
+
+
+
+const arr = new Array();
+
+arr.push(10);
+arr.push(20);
+arr.push(30);
+arr.push(40);
+arr.push(50);
+
+console.log(app.x);
+console.log(app.y);
+console.log(app);
+
+app.print(arr);
+
+const newArr = arr.filter((el) => el > 20);
+
+console.log(newArr);
+console.log(arr);
+
+const arr = [1, 2, 33, 55, 77, 101, 501];
+
+const binarySearch = (arr, val) => {
+  let s = 0;
+  let e = arr.length - 1;
+
+  while (s <= e) {
+    let mid = s + (e - s) / 2;
+
+    if (arr[mid] == val) {
+      return mid;
+    } else if (val > arr[mid]) {
+      s = mid + 1;
+    } else {
+      e = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(binarySearch(arr, 500));
+
+*/
