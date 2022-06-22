817826040a5c732317d6f86133505707b11fa483
vedant <ursvedantyetekar@gmail.com>
2022-06-23 00:38:00 +0530

    added 01-Knapsack soln

--- leetcode-gfg-interviewbit/gfg-solutions/0 - 1 Knapsack Problem.cpp
+++ leetcode-gfg-interviewbit/gfg-solutions/0 - 1 Knapsack Problem.cpp
@@ -1,5 +1,7 @@
 // https : // practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1#
 
+// Recursive
+
 class Solution
 {
 public:
@@ -24,5 +26,81 @@ public:
     int knapSack(int W, int wt[], int val[], int n)
     {
         return helper(W, wt, val, n - 1);
+    }
+};
+
+// Memoization
+
+class Solution
+{
+public:
+    int helper(int W, int wt[], int val[], int n, vector<vector<int>> &dp)
+    {
+        if (n == 0 or W == 0)
+        {
+            return 0;
+        }
+
+        if (dp[n][W] != -1)
+        {
+            return dp[n][W];
+        }
+
+        else if (W < wt[n - 1])
+        {
+            return dp[n][W] = helper(W, wt, val, n - 1, dp);
+        }
+
+        else
+        {
+            return dp[n][W] = max(val[n - 1] + helper(W - wt[n - 1], wt, val, n - 1, dp), helper(W, wt, val, n - 1, dp));
+        }
+    }
+
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+
+        int max_profit = helper(W, wt, val, n, dp);
+
+        return max_profit;
+    }
+};
+
+// DP
+
+class Solution
+{
+public:
+    int helper(int W, int wt[], int val[], int n, vector<vector<int>> &dp)
+    {
+        if (n == 0 or W == 0)
+        {
+            return 0;
+        }
+
+        if (dp[n][W] != -1)
+        {
+            return dp[n][W];
+        }
+
+        else if (W < wt[n - 1])
+        {
+            return dp[n][W] = helper(W, wt, val, n - 1, dp);
+        }
+
+        else
+        {
+            return dp[n][W] = max(val[n - 1] + helper(W - wt[n - 1], wt, val, n - 1, dp), helper(W, wt, val, n - 1, dp));
+        }
+    }
+
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+
+        int max_profit = helper(W, wt, val, n, dp);
+
+        return max_profit;
     }
 };
