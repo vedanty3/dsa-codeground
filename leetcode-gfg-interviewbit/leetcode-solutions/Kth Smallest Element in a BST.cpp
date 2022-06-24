e2a55c270a6a297451970447a32c699f7dded460
vedant <ursvedantyetekar@gmail.com>
2022-06-24 12:10:44 +0530

    added gfg soln

--- leetcode-gfg-interviewbit/leetcode-solutions/Kth Smallest Element in a BST.cpp
+++ leetcode-gfg-interviewbit/leetcode-solutions/Kth Smallest Element in a BST.cpp
@@ -40,3 +40,72 @@ public:
         }
     }
 };
+
+/*
+ * TC: O(N) & SC: O(1)
+ * Definition for a binary tree node.
+ * struct TreeNode
+{
+    *int val;
+    *TreeNode *left;
+    *TreeNode *right;
+    *TreeNode() : val(0), left(nullptr), right(nullptr){} * TreeNode(int x) : val(x), left(nullptr), right(nullptr){} * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+                                                                                                                                                                                                   *
+};
+ */
+
+class Solution
+{
+public:
+    int kthSmallest(TreeNode *root, int k)
+    {
+        int find_k = k;
+        int kth_smallest;
+
+        TreeNode *curr = root;
+
+        while (curr != NULL)
+        {
+            if (curr->left == NULL)
+            {
+                find_k--;
+
+                if (find_k == 0)
+                {
+                    kth_smallest = curr->val;
+                }
+
+                curr = curr->right;
+            }
+            else
+            {
+                TreeNode *prev = curr->left;
+
+                while (prev->right != NULL and prev->right != curr)
+                {
+                    prev = prev->right;
+                }
+
+                if (prev->right == NULL)
+                {
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else
+                {
+                    find_k--;
+
+                    if (find_k == 0)
+                    {
+                        kth_smallest = curr->val;
+                    }
+
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return kth_smallest;
+    }
+};
