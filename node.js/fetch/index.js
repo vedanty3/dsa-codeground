6c255de7000876cf14289b74128aa4c685577aa5
vedant <ursvedantyetekar@gmail.com>
2022-07-08 02:16:31 +0530

    added fetch qpi tutorial

---
+++ node.js/fetch/index.js
@@ -0,0 +1,17 @@
+// FETCH API METHOD
+
+const btn = document.getElementById("get_data");
+
+const rawData = () =>
+  fetch("https://dummyjson.com/quotes")
+    .then((response) => response.json())
+    .then((data) => {
+      const quotesArr = data.quotes;
+      const randInt = Math.floor(Math.random() * 31);
+      document.getElementById("data").innerHTML = quotesArr[randInt].quote;
+      document.getElementById(
+        "by"
+      ).innerHTML = `... ${quotesArr[randInt].author}`;
+    });
+
+btn.addEventListener("click", rawData);
