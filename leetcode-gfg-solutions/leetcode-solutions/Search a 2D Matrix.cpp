163a97be2457a47e18d0c1fccf01e24a4f935199
vedant <ursvedantyetekar@gmail.com>
2022-07-09 23:40:52 +0530

    added gfg soln

--- leetcode-gfg-solutions/leetcode-solutions/Search a 2D Matrix.cpp
+++ leetcode-gfg-solutions/leetcode-solutions/Search a 2D Matrix.cpp
@@ -5,34 +5,18 @@ class Solution
 public:
     bool searchMatrix(vector<vector<int>> &matrix, int target)
     {
+        int m = matrix.size(), n = matrix[0].size();
+        int low = 0, high = m * n - 1;
 
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        int start = 0;
-        int end = (m * n) - 1;
-
-        if (matrix.size() == 0)
+        while (low <= high)
         {
-            return false;
-        }
-
-        while (start <= end)
-        {
-            int mid = start + (end - start) / 2;
-
+            int mid = low + (high - low) / 2;
             if (matrix[mid / n][mid % n] == target)
-            {
                 return true;
-            }
-            else if (matrix[mid / n][mid % n] < target)
-            {
-                start = mid + 1;
-            }
+            else if (matrix[mid / n][mid % n] > target)
+                high = mid - 1;
             else
-            {
-                end = mid - 1;
-            }
+                low = mid + 1;
         }
 
         return false;
