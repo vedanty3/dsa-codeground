10366a398a94d0ca0e357790520627a1ef05f8f9
vedant <ursvedantyetekar@gmail.com>
2022-07-10 21:02:35 +0530

    added leetcode and gfg soln

--- leetcode-gfg-solutions/leetcode-solutions/Climbing Stairs.cpp
+++ leetcode-gfg-solutions/leetcode-solutions/Climbing Stairs.cpp
@@ -1,52 +1,46 @@
-// https : // leetcode.com/problems/climbing-stairs/
-
+// Memoization
 class Solution
 {
 public:
+    int helper(int n, vector<int> &dp)
+    {
+        if (n == 0 or n == 1)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+    }
+
     int climbStairs(int n)
     {
-
-        int *dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++)
-        {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        int result = dp[n];
-        delete[] dp;
-        return result;
+        vector<int> dp(n + 1, -1);
+        return helper(n, dp);
     }
 };
 
-// Approach 2
+// Recursion
 class Solution
 {
 public:
     int climbStairs(int n)
     {
+        if (n == 0 or n == 1)
+            return 1;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
 
-        int k = 2;
-        int *dp = new int[n + 1];
+// DP
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1);
         dp[0] = 1;
-
-        for (int i = 1; i <= n; i++)
-        {
-            int ans = 0;
-
-            for (int j = 1; j <= k; j++)
-            {
-                if ((i - j) >= 0)
-                {
-                    ans += dp[i - j];
-                }
-            }
-
-            dp[i] = ans;
-        }
-
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
         return dp[n];
     }
 };
