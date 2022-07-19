4584087fb7ba0103dbecb74a01736e9a56751d83
vedant <ursvedantyetekar@gmail.com>
2022-07-19 19:10:10 +0530

    added number theory concepts

---
+++ number theory/5.xor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    /*
+    // x^x = 0
+    // x^0 = x
+
+    // swap two numbers
+    int a = 1, b = 2;
+    a = a ^ b, b = a ^ b, a = a ^ b;
+    cout << "a: " << a << " "
+         << "b: " << b << "\n";
+
+    // find number with odd count
+    int arr[] = {1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 2, 4};
+    int xorr = 0;
+    for (int i = 0; i < 13; i++)
+        xorr ^= arr[i];
+    cout << xorr << "\n";
+    */
+
+    return 0;
+}
