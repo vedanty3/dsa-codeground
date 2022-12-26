c231514d0074a3e87ce879e3794e78ba73b69e54
vedant <ursvedantyetekar@gmail.com>
2022-12-26 23:05:12 +0530

    added merge sort

---
+++ strivers-series/recursion/Merge Sort.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define si set<int>
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpi vector<pii>
+#define vpp vector<pair<int, pii>>
+#define mii map<int, int>
+#define mpi map<pii, int>
+#define spi set<pii>
+#define endl "\n"
+#define sz(x) ((int)x.size())
+#define all(p) p.begin(), p.end()
+#define double long double
+#define que_max priority_queue<int>
+#define que_min priority_queue<int, vi, greater<int>>
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define print(a)          \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+#define print1(a)    \
+    for (auto x : a) \
+    cout << x.F << " " << x.S << endl
+#define print2(a, x, y)         \
+    for (int i = x; i < y; i++) \
+        cout << a[i] << " ";    \
+    cout << endl
+
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1)
+            x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+
+const int N = 200005;
+
+void merge(vector<int> &arr, int start, int end)
+{
+    int mid = start + (end - start) / 2;
+    int i = start;
+    int k = start;
+    int j = mid + 1;
+
+    vector<int> tmp(arr.size(), 0);
+
+    while (i <= mid and j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            tmp[k++] = arr[i++];
+        }
+        else
+        {
+            tmp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid)
+    {
+        tmp[k++] = arr[i++];
+    }
+
+    while (j <= end)
+    {
+        tmp[k++] = arr[j++];
+    }
+
+    for (int i = start; i <= end; ++i)
+    {
+        arr[i] = tmp[i];
+    }
+}
+
+void mergeSort(vector<int> &arr, int start, int end)
+{
+    if (start >= end)
+        return;
+    int mid = start + (end - start) / 2;
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, start, end);
+}
+
+void solve()
+{
+    vi arr;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int curr = -1;
+        cin >> curr;
+        arr.pb(curr);
+    }
+
+    cout << "Before sorting: ";
+    print(arr);
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    cout << "After sorting: ";
+    print(arr);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    clock_t z = clock();
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+}
